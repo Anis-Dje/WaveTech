41265ba1428074d9908c4a60050793a29473b87b
Anis-Dje <anisleprince2005@gmail.com>
2025-11-21 10:24:47 +0100

    Fix user creation by overriding create method in serializer

--- backend/wavetech_backend/serializers.py
+++ backend/wavetech_backend/serializers.py
@@ -8,3 +8,17 @@ class CustomUserCreateSerializer(UserCreatePasswordRetypeSerializer):
     class Meta(UserCreatePasswordRetypeSerializer.Meta):
         model = User
         fields = ('id', 'email', 'username', 'first_name', 'last_name', 'password', 're_password')
+    
+    def create(self, validated_data):
+        # Remove re_password as it's not needed for user creation
+        validated_data.pop('re_password', None)
+        
+        # Create user with all required fields
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', '')
+        )
+        return user
