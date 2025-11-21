67d5883eeb8aeb7cdf73c65fa095ed3c948fce3c
Anis-Dje <anisleprince2005@gmail.com>
2025-11-21 16:03:51 +0100

    fix: add webstar-electro.com and common image CDN domains to next.config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -11,12 +11,24 @@ const nextConfig = {
         hostname: '**.unsplash.com',
       },
       {
+        protocol: 'https',
+        hostname: 'webstar-electro.com',
+      },
+      {
         protocol: 'http',
         hostname: 'localhost',
       },
       {
         protocol: 'https',
         hostname: 'wavetech.onrender.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.cloudinary.com',
       },
     ],
   },
